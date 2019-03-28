8598fa768c358384e344290a110ffcad5c98e298
LiamThys <38426825+LiamThys@users.noreply.github.com>
2019-03-28 13:38:50 +0100

    snek

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+}
+
+function draw() {
+}
+
+//functie om een random locatie te vinden voor een stuk eten
+function pickLocation() {
+}
+
+//functie die zegt naar welke richting de snake moet
+function keyPressed() {
+  }
+}
